9768020cb8c64af3dc8edba56527863906893b67
Eloy Duran <eloy.de.enige@gmail.com>
2011-07-07 11:42:27 +0200

    + app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appsterdam::Application.config.secret_token = 'ce4d39f3156ffd31f87ac571df2372bef3181bd5479033846f42f2bbcc01568e390d86c84ffd29147862cdb1c52040cb6c21339fd37ac265778fb035fc014ccb'
