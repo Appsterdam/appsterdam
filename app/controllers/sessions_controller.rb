cce6109747fb893d5f71fa71a98590276c7a5b9d
Manfred Stienstra <manfred@fngtps.com>
2011-07-07 14:27:57 +0200

    Keep the OAuth request token and secret around for checking the authorization.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
   def new
     @request_token = twitter_client.authentication_request_token(:oauth_callback => session_url)
+    session[:token] = @request_token.token
+    session[:token_secret] = @request_token.secret
     redirect_to @request_token.authorize_url
   end
   
--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -2,12 +2,14 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class FakeTwitter
   class FakeRequestToken
-    attr_accessor :authorize_url
+    attr_accessor :authorize_url, :token, :secret
   end
   
   def authentication_request_token(options={})
     request_token = FakeRequestToken.new
     request_token.authorize_url = 'http://fake.twitter.com/as32df76'
+    request_token.token = 'iu87er56'
+    request_token.secret = 'hj76df87'
     request_token
   end
 end
@@ -17,6 +19,8 @@ describe "On the", SessionsController, "a visitor" do
     controller.stubs(:twitter_client).returns(fake_twitter)
     get :new
     should.redirect_to assigns(:request_token).authorize_url
+    session[:token].should.not.be.blank
+    session[:token_secret].should.not.be.blank
   end
   
   private
