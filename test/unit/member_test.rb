05aa5005b8d821b99ab3c50f3bddc644db0b128a
Eloy Duran <eloy.de.enige@gmail.com>
2011-07-07 13:50:25 +0200

    Write a few tests regarding twitter_id validation.

--- test/unit/member_test.rb
+++ test/unit/member_test.rb
@@ -1,8 +1,19 @@
 require File.expand_path("../../test_helper", __FILE__)
 
-describe "A", Member do
-  it "fetches profile data from twitter.com" do
-    @member = Member.new
-    @member.fetch_profile_data!
+describe Member, "concerning validation" do
+  before do
+    @member = members(:developer)
+  end
+
+  it "requires a twitter_id" do
+    @member.twitter_id = ''
+    @member.should.be.invalid
+    @member.errors[:twitter_id].should.not.be.blank
+  end
+
+  it "requires a unique twitter_id" do
+    duplicate = Member.new(:twitter_id => @member.twitter_id)
+    duplicate.should.be.invalid
+    duplicate.errors[:twitter_id].should.not.be.blank
   end
 end
