85101d5eff941953639efd77a046b0a6cd8e8cdd
Eloy Duran <eloy.de.enige@gmail.com>
2011-07-07 13:56:08 +0200

    Add a few more failing specs.

--- test/unit/member_test.rb
+++ test/unit/member_test.rb
@@ -17,3 +17,22 @@ describe Member, "concerning validation" do
     duplicate.errors[:twitter_id].should.not.be.blank
   end
 end
+
+describe Member do
+  it "creates a new record with the JSON data returned by twitter.com" do
+    expected = members(:developer); expected.delete
+    created = Member.create_with_profile_data(File.read(fixture('TwitterAPI/users-show.json')))
+    created.should == expected
+  end
+end
+
+describe "A", Member do
+  before do
+    @member = members(:developer)
+  end
+
+  it "updates its profile data from twitter.com" do
+    @member = Member.new
+    @member.fetch_profile_data!
+  end
+end
