b6455506b204b9dd13097b7ae851745dee229e1f
Eloy Duran <eloy.de.enige@gmail.com>
2011-07-12 11:47:28 +0200

    Add validations to SpamMarking

--- app/models/spam_marking.rb
+++ app/models/spam_marking.rb
@@ -1,4 +1,8 @@
 class SpamMarking < ActiveRecord::Base
   belongs_to :member
   belongs_to :reporter, :class_name => 'Member'
+
+  private
+
+  validates_presence_of :member_id, :ip_address
 end
--- test/unit/spam_marking_test.rb
+++ test/unit/spam_marking_test.rb
@@ -1,8 +1,26 @@
 require File.expand_path("../../test_helper", __FILE__)
 
+describe SpamMarking, "concerning validation" do
+  before do
+    @marking = members(:developer).spam_markings.build(:ip_address => '1.2.3.4')
+  end
+
+  it "is invalid without a `member'" do
+    @marking.member = nil
+    @marking.should.be.invalid
+    @marking.errors[:member_id].should.not.be.blank
+  end
+
+  it "is invalid without an `ip_address'" do
+    @marking.ip_address = ''
+    @marking.should.be.invalid
+    @marking.errors[:ip_address].should.not.be.blank
+  end
+end
+
 describe "A", SpamMarking do
   before do
-    @marking = members(:developer).spam_markings.build
+    @marking = members(:developer).spam_markings.build(:ip_address => '1.2.3.4')
   end
 
   it "returns the member that's reported as being spam" do
