961eb8d84a6d606cf9f081244b0f969c02e2cc97
Klaas Speller <klaasspeller@gmail.com>
2011-07-22 18:42:54 +0200

    Update seed data script to add extra attributes

--- script/seed_members.rb
+++ script/seed_members.rb
@@ -4,6 +4,29 @@ Dir.glob(Rails.root + 'db/seeds/*.json').each do |file|
   users = list['users']
   users.each do |attributes|
     member = Member.find_by_twitter_id(attributes['id'].to_s) || Member.create_with_twitter_user_attributes(attributes)
+
+    # TODO Check for better way of setting properties in seed
+    member.entity << case list_name
+    when "companies" then "company"
+    when "students" then "student"
+    when "collectives" then "group"
+    else
+      "individual"
+    end
+
+    member.work_location = "appsterdam" if list_name == "appsterdammers"
+
+    
+    member.work_types = member.work_types.push 
+    
+    member.work_types = member.work_types << case list_name
+    when "designers" then "design"
+    when "developers" then "developer"
+    when "marketeers" then "marketing"
+    else
+      nil
+    end
+
     member.save
   end
 end
