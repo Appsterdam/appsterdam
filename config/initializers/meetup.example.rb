241bc3df190c25e8e5e885d424fd1b0b2132a8bf
Felix Hageloh <felix.hageloh@gmail.com>
2011-08-24 18:56:24 +0200

    added example meetup config file

---
+++ config/initializers/meetup.example.rb
@@ -0,0 +1,6 @@
+# get your meetup API key here: http://www.meetup.com/meetup_api/key/
+Appsterdam::Application.meetup_api_options = {
+  :events_url => "https://api.meetup.com/2/events",
+  :key        => "<<<  YOUR API KEY >>>",
+  :group_name => "Appsterdam"
+}
