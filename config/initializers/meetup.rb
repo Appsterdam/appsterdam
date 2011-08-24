ab9ee6495b540208e3a2fdff74a0bff29144c40d
Felix Hageloh <felix.hageloh@gmail.com>
2011-08-24 18:55:51 +0200

    removed config files for twitter and meetup that need to be configured per developer

--- config/initializers/meetup.rb
+++
@@ -1,5 +0,0 @@
-Appsterdam::Application.meetup_api_options = {
-  :events_url => "https://api.meetup.com/2/events",
-  :key        => "1179f486f313f711a1f74381be5a2b",
-  :group_name => "Appsterdam"
-}
--- config/initializers/twitter.rb
+++
@@ -1,4 +0,0 @@
-Appsterdam::Application.twitter_options = {
-  :consumer_key    => 'FOVYE6I6HayDznSZCQJ9g',
-  :consumer_secret => 'Awbj6nHkJ8kCnrrx87eEi50miFufbY3GehFglaw60'
-}
