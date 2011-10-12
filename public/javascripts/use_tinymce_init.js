bbb666f44d9cd49458dd1af7474cb80966f2a1bf
Igor Sutton <igorsutton@me.com>
2011-10-12 11:15:12 +0200

    using advanced tinymce now

--- public/javascripts/use_tinymce_init.js
+++ public/javascripts/use_tinymce_init.js
@@ -1,6 +1,16 @@
-// This is copied directly from http://tinymce.moxiecode.com/wiki.php/%22For_Dummies%22
+// This is taken directly from http://tinymce.moxiecode.com/wiki.php/%22For_Dummies%22
 tinyMCE.init({
         mode: "specific_textareas",
         editor_deselector: "no-tinymce",
-        theme : "simple"    //(n.b. no trailing comma, this will be critical as you experiment later)
+        theme : "advanced",
+        plugins : "emotions,spellchecker,advhr,insertdatetime,preview", 
+                
+        // Theme options - button# indicated the row# only
+        theme_advanced_buttons1 : "newdocument,|,bold,italic,underline,|,justifyleft,justifycenter,justifyright,fontselect,fontsizeselect,formatselect",
+        theme_advanced_buttons2 : "cut,copy,paste,|,bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,anchor,image,|,code,preview,|,forecolor,backcolor",
+        theme_advanced_buttons3 : "insertdate,inserttime,|,spellchecker,advhr,,removeformat,|,sub,sup,|,charmap,emotions",      
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location : "bottom",
+        theme_advanced_resizing : true
 });
